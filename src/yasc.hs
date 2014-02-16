6bce2e8432cfe02419e48334f48be36c5210ede8
Richo Healey <richo@psych0tik.net>
2014-02-15 20:21:32 -0800

    Setup the special export form

--- src/yasc.hs
+++ src/yasc.hs
@@ -88,6 +88,13 @@ getVar envRef var = do env <- liftIO $ readIORef envRef
                              (liftIO . readIORef)
                              (M.lookup var env)
 
+setVar :: Env -> String -> LispVal -> IO LispVal
+setVar envRef var value = do env <- liftIO $ readIORef envRef
+                             valueRef <- newIORef value
+                             env <- readIORef envRef
+                             writeIORef envRef (M.insert var valueRef env)
+                             return value
+
 defineVar :: Env -> String -> LispVal -> IO LispVal
 defineVar envRef var value = do
     alreadyDefined <- liftIO $ isBound envRef var
@@ -118,12 +125,23 @@ makeFunc params body = Func (map showVal params) body
 makeNormalFunc = makeFunc
 makeIntrinsic params body = Intrinsic (map showVal params) body
 
+withNewExport :: LispVal -> LispVal -> LispVal
+withNewExport (List contents) (String new) = List ( String new : contents )
+
+exportName :: Env -> String -> IO LispVal
+exportName env name = do
+    exports <- getVar env "exports"
+    setVar env "exports" $ withNewExport exports (String name)
+
 topLevelEval :: Env -> LispVal -> IO LispVal
 -- TODO Consider making this support some of:
 -- (intrinsic! (foo bar) ...)
 -- (intrinsic! (foo bar . rest) ...)
-topLevelEval env (List (Atom "intrinsic!" : Atom name: List params : body)) =
+topLevelEval env (List (Atom "intrinsic!" : Atom name : List params : body)) =
     defineVar env name $ makeIntrinsic params body
+topLevelEval env (List [Atom "export", Atom name]) =
+    -- TODO Should also barf if you try to export undefined names
+    liftIO $ exportName env name
 topLevelEval env (List [Atom "define", Atom var, form]) = do
     ret <- eval env form
     val <- defineVar env var ret
